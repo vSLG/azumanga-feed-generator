0f4d8ac3ec6adcdec62fd79b91aae730bd39599e
vslg <slgf@protonmail.ch>
2023-10-01 21:56:45 -0300

    Add IoT feed

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -6,6 +6,7 @@ import {
 import { Algo } from './algo'
 import { AzumangaDaiohAlgo } from './azumanga-daioh'
 import { CirnoAlgo } from './cirno'
+import { IoTAlgo } from './iot'
 import { MarisaKirisameAlgo } from './marisa-kirisame'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
@@ -14,6 +15,7 @@ export const algos: Array<Algo> = [
   new AzumangaDaiohAlgo(0),
   new CirnoAlgo(1),
   new MarisaKirisameAlgo(2),
+  new IoTAlgo(3),
 ]
 
 export const algosByShortname: Record<string, Algo> = algos.reduce((acc, algo) => ({ ...acc, [algo.shortname]: algo }), {})
---
+++ src/algos/iot.ts
@@ -0,0 +1,29 @@
+import { Algo } from './algo'
+import { IncomingPost } from '../util/types'
+
+export class IoTAlgo extends Algo {
+  constructor(feed: number) {
+    super(feed)
+
+    this.shortname = 'iot'
+    this.displayname = 'IoT Feed'
+    this.description = 'This feed filters IoT content from the whole network' +
+      `\n(The regex is so big I won't be writing it here)`
+    this.avatar = './avatars/espressif.png'
+    this.publish = true
+  }
+
+  public re = /((smart|connected) (devices|cities|city|transportation|agriculture|home|car)|home automation|(industrial )?iot|iiot|wearables|edge computing|iot (security|protocol|gateway|architecture|challenge|standard|solution|research)|mqtt|cloud computing|data (analysis|analytics)|real.?time monitoring|machine.?to.?machine|m2m|embedded system|(aws|google cloud|azure|cellular) iot|telemetry|lpwan|zigbee|bluetooth( low energy)?|ble|interoperability|firmware|esp32|espressif|raspberry.?pi|rpi|esp.?idf)/i
+
+  public async filterPost(post: IncomingPost): Promise<boolean> {
+    const textMatches = this.re.test(post.text)
+    let imageMatches = false
+
+    const { embed } = post
+
+    if (embed?.images?.some(image => this.re.test(image.alt)))
+      imageMatches = true
+
+    return textMatches || imageMatches
+  }
+}
